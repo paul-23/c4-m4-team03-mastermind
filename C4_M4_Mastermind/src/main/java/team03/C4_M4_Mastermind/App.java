2aaad38ad581aa8f753d111f2fb49dfa05908781
Paul S <62121921+paul-23@users.noreply.github.com>
2023-04-04 09:11:50 +0200

    Crear proyecto inicial

---
+++ C4_M4_Mastermind/src/main/java/team03/C4_M4_Mastermind/App.java
@@ -0,0 +1,13 @@
+package team03.C4_M4_Mastermind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
