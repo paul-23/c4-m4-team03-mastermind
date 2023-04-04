402275388ff6efda110bc139eccad2ab4ecc8bdc
Paul S <62121921+paul-23@users.noreply.github.com>
2023-04-04 13:10:31 +0200

    Cambio logica aplicacion vista

--- C4_M4_Mastermind/src/main/java/team03/C4_M4_Mastermind/vista.java
+++ C4_M4_Mastermind/src/main/java/team03/C4_M4_Mastermind/vista.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 
@@ -14,74 +15,44 @@ public class vista extends JFrame {
 
 	public vista(int intentos, int colores) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 564, 323);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		JButton btn1 = new JButton("");
+		btn1.setBounds(27, 25, 89, 23);
+		contentPane.add(btn1);
+
+		JButton btn2 = new JButton("");
+		btn2.setBounds(126, 25, 89, 23);
+		contentPane.add(btn2);
+
+		JButton btn3 = new JButton("");
+		btn3.setBounds(225, 25, 89, 23);
+		contentPane.add(btn3);
+
+		JButton btn4 = new JButton("");
+		btn4.setBounds(335, 25, 89, 23);
+		contentPane.add(btn4);
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setBounds(27, 25, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setBounds(126, 25, 89, 23);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setBounds(225, 25, 89, 23);
-		contentPane.add(btnNewButton_2);
-		
-		JButton btnNewButton_2_1 = new JButton("");
-		btnNewButton_2_1.setBounds(335, 25, 89, 23);
-		contentPane.add(btnNewButton_2_1);
-		
-		colores = 4;
-	    Color array[] = new Color[colores];
-	    Color[] arrCol = {Color.RED,Color.pink,Color.yellow,Color.green,Color.black,Color.gray,Color.blue,Color.orange,Color.magenta,Color.DARK_GRAY};
-	  
-	    setVisible(true);
-	    
-		int i = 0;
-		if( i <= 1) {
-		int randomNumber = (int) Math.round(Math.random()*colores);
+		setVisible(true);
+
+		Color array[] = new Color[colores];
+		Color[] arrCol = { Color.RED, Color.pink, Color.yellow, Color.green, Color.black, Color.gray,
+				Color.blue, Color.orange, Color.magenta, Color.cyan };
+		JButton[] btnSelecionados = { btn1, btn2, btn3, btn4 };
+
+		for (int i = 0; i < colores; i++) {
+			int randomNumber = (int) Math.round(Math.random() * 9);
 			Color colorselecc = arrCol[randomNumber];
-			btnNewButton.setBackground(colorselecc);
-	        array[i] = colorselecc;
-
-			i++;
+			array[i] = colorselecc;
 		}
-		 i = 0;
-		if( i <= 1) {
-			int randomNumber = (int) Math.round(Math.random()*colores);
-				Color colorselecc = arrCol[randomNumber];
-				btnNewButton_1.setBackground(colorselecc);
-		        array[i] = colorselecc;
-
-				i++;
-			}
-		 i = 0;
-		if( i <= 1) {
-			int randomNumber = (int) Math.round(Math.random()*colores);
-				Color colorselecc = arrCol[randomNumber];
-				btnNewButton_2.setBackground(colorselecc);
-		        array[i] = colorselecc;
-
-				i++;
-			}
-		 i = 0;
-		if( i <= 1) {
-			int randomNumber = (int) Math.round(Math.random()*colores);
-				Color colorselecc = arrCol[randomNumber];
-				btnNewButton_2_1.setBackground(colorselecc);
-		        array[i] = colorselecc;
-
-				i++;
-			}
-}	
+		
+		for (int i = 0; i < btnSelecionados.length; i++) {
+			btnSelecionados[i].setBackground(array[i]);
+		}
 	}
-
-	
-
-
+}
