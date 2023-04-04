2e6f7a1acae886257e5ca5c8d4fdc7da6bdc520c
Paul S <62121921+paul-23@users.noreply.github.com>
2023-04-04 21:10:27 +0200

    Creación del menu bar con diferentes opciones

--- C4_M4_Mastermind/src/main/java/team03/C4_M4_Mastermind/vista.java
+++ C4_M4_Mastermind/src/main/java/team03/C4_M4_Mastermind/vista.java
@@ -1,23 +1,40 @@
 package team03.C4_M4_Mastermind;
 
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JMenu;
 
 public class vista extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JButton btnColor1, btnColor2, btnColor3, btnColor4, btnColor5, btnColor6;
+	private JButton btn1, btn2, btn3, btn4;
+	private JCheckBoxMenuItem chckbxmntmMostrarSolucion;
+	private JPanel panelColores, panelSolucion;
+	private JMenuItem mntmAyuda, mntmInformacion;
 
 	public vista(int intentos, int colores) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,64 +44,94 @@ public class vista extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Combinaci\u00F3n secreta", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.setBounds(713, 130, 252, 68);
-		contentPane.add(panel);
-		panel.setLayout(null);
 
-		JButton btn1 = new JButton("");
+		panelSolucion = new JPanel();
+		panelSolucion.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)),
+				"Combinaci\u00F3n secreta", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelSolucion.setBounds(717, 176, 252, 68);
+		contentPane.add(panelSolucion);
+		panelSolucion.setLayout(null);
+
+		btn1 = new JButton("");
 		btn1.setBounds(10, 23, 30, 30);
-		panel.add(btn1);
+		panelSolucion.add(btn1);
 
-		JButton btn2 = new JButton("");
+		btn2 = new JButton("");
 		btn2.setBounds(50, 23, 30, 30);
-		panel.add(btn2);
+		panelSolucion.add(btn2);
 
-		JButton btn3 = new JButton("");
+		btn3 = new JButton("");
 		btn3.setBounds(90, 23, 30, 30);
-		panel.add(btn3);
+		panelSolucion.add(btn3);
 
-		JButton btn4 = new JButton("");
+		btn4 = new JButton("");
 		btn4.setBounds(130, 23, 30, 30);
-		panel.add(btn4);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Colores disponibles", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1.setBounds(713, 51, 252, 68);
-		contentPane.add(panel_1);
-		
+		panelSolucion.add(btn4);
+
+		panelColores = new JPanel();
+		panelColores.setLayout(null);
+		panelColores.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)),
+				"Colores disponibles", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelColores.setBounds(717, 86, 252, 68);
+		contentPane.add(panelColores);
+
 		btnColor1 = new JButton("");
 		btnColor1.setBounds(10, 23, 30, 30);
-		panel_1.add(btnColor1);
-		btnColor1.setVisible( false );
-		
+		panelColores.add(btnColor1);
+		btnColor1.setVisible(false);
+
 		btnColor2 = new JButton("");
 		btnColor2.setBounds(50, 23, 30, 30);
-		panel_1.add(btnColor2);
-		btnColor2.setVisible( false );
-		
+		panelColores.add(btnColor2);
+		btnColor2.setVisible(false);
+
 		btnColor3 = new JButton("");
 		btnColor3.setBounds(90, 23, 30, 30);
-		panel_1.add(btnColor3);
-		btnColor3.setVisible( false );
-		
+		panelColores.add(btnColor3);
+		btnColor3.setVisible(false);
+
 		btnColor4 = new JButton("");
 		btnColor4.setBounds(130, 23, 30, 30);
-		panel_1.add(btnColor4);
-		btnColor4.setVisible( false );
-		
+		panelColores.add(btnColor4);
+		btnColor4.setVisible(false);
+
 		btnColor5 = new JButton("");
 		btnColor5.setBounds(170, 23, 30, 30);
-		panel_1.add(btnColor5);
-		btnColor5.setVisible( false );
-		
+		panelColores.add(btnColor5);
+		btnColor5.setVisible(false);
+
 		btnColor6 = new JButton("");
 		btnColor6.setBounds(210, 23, 30, 30);
-		panel_1.add(btnColor6);
-		btnColor6.setVisible( false );
+		panelColores.add(btnColor6);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(255, 255, 255));
+		menuBar.setBounds(0, 0, 1026, 31);
+		contentPane.add(menuBar);
+
+		JMenu mnOpciones = new JMenu("Opciones");
+		menuBar.add(mnOpciones);
+
+		chckbxmntmMostrarSolucion = new JCheckBoxMenuItem("Mostrar Solución");
+		chckbxmntmMostrarSolucion.setSelected(true);
+		mnOpciones.add(chckbxmntmMostrarSolucion);
+		chckbxmntmMostrarSolucion.addActionListener(mostrarSolucion);
+
+		JMenu mnAbout = new JMenu("Ayuda");
+		menuBar.add(mnAbout);
+
+		mntmAyuda = new JMenuItem("Ayuda");
+		mnAbout.add(mntmAyuda);
+		
+		mntmAyuda.addActionListener(mostrarAyuda);
+
+		mntmInformacion = new JMenuItem("Información");
+		mnAbout.add(mntmInformacion);
+		btnColor6.setVisible(false);
+
+		mntmInformacion.addActionListener(mostrarInformacion);
 
 		setVisible(true);
 
@@ -96,7 +143,8 @@ public class vista extends JFrame {
 		JButton[] btnSelecionados = { btn1, btn2, btn3, btn4 };
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-		// Generamos los colores según la dificultad escojida, con colores unicos del array
+		// Generamos los colores según la dificultad escojida, con colores unicos del
+		// array
 		int num = 0;
 		while (numbers.size() < colores) {
 			int randomNumber = (int) Math.round(Math.random() * 9);
@@ -104,20 +152,55 @@ public class vista extends JFrame {
 				numbers.add(randomNumber);
 				arrayDificultad[num] = arrCol[randomNumber];
 				btnColoresDisponibles[num].setBackground(arrayDificultad[num]);
-				btnColoresDisponibles[num].setVisible( true );
+				btnColoresDisponibles[num].setVisible(true);
 				num++;
 			}
 		}
 
-		// Generaamos los colores escojidos por le ordenador como solucion, puede contener dos valores iguales
+		// Generaamos los colores escojidos por le ordenador como solucion, puede
+		// contener dos valores iguales
 		for (int i = 0; i < arraySolucion.length; i++) {
 			int randomNumber = (int) Math.round(Math.random() * (arraySolucion.length - 1));
 			Color colorSelec = arrayDificultad[randomNumber];
 			arraySolucion[i] = colorSelec;
-			
+
 			// Asignamos los colores a los botones
 			btnSelecionados[i].setBackground(arraySolucion[i]);
 		}
 
 	}
+
+	ActionListener mostrarSolucion = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if (chckbxmntmMostrarSolucion.isSelected()) {
+				panelSolucion.setVisible(true);
+			} else {
+				panelSolucion.setVisible(false);
+			}
+		}
+	};
+
+	// Cremos un metodo que no muestra un mensaje dentro de una alerta al pulsar el
+	// botón
+	ActionListener mostrarInformacion = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			JLabel label = new JLabel("<html><h1>MASTER MIND</h1>"
+					+ "<p>Aplicaión desarrollada por el team 03 (Alejandro, Arnau y Paul)</p>"
+					+ "<p>Más información en el repositorio de git</p>"
+					+ "<p style=\"color:blue\">https://github.com/paul-23/c4-m4-team03-mastermind</p>" + "</html>");
+			label.setFont(new Font("Arial", Font.BOLD, 20)); // Cambiamos la fuente y tamaño del texto
+			JOptionPane.showMessageDialog(contentPane, label);
+		}
+	};
+
+	ActionListener mostrarAyuda = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			try {
+				Desktop.getDesktop().browse(new URI("https://en.wikipedia.org/wiki/Mastermind_(board_game)"));
+
+			} catch (IOException | URISyntaxException e1) {
+				e1.printStackTrace();
+			}
+		}
+	};
 }
